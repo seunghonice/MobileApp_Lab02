c03dfe1cdf4e71082b27028c17febfba6c0308e4
HongSeungho <seunghonice@gmail.com>
2017-03-09 13:22:48 +0900

    Change mainlayout.xml and complete MainActivity.java

--- app/src/main/java/com/mobile/lab02/MainActivity.java
+++ app/src/main/java/com/mobile/lab02/MainActivity.java
@@ -2,14 +2,30 @@ package com.mobile.lab02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    EditText age1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hi everyone", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.mainlayout);
+        btn1 = (Button) findViewById(R.id.btn_calcAge);
+        age1 = (EditText) findViewById(R.id.age);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "당신의 나이는 " + (2017 - Integer.parseInt(age1.getText().toString()) + 1) + "입니다", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
